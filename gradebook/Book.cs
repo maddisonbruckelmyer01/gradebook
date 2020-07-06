1174975ded0f1b49cd6a8f1f60db0cda7e5f8cbd
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-06 12:26:31 -0500

    adds testing and cleans up program.cs

--- gradebook/Book.cs
+++ gradebook/Book.cs
@@ -1,14 +1,48 @@
 ﻿using System.Collections.Generic;
+using System;
+
 
 namespace gradebook
 {
     class Book
     {
+
+        public Book(string name) 
+         {
+            grades = new List<double>();
+            this.name = name; 
+        }
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
 
-        List<double> grades;
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+
+            var highGrade = double.MinValue;
+
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                // if (number > highGrade) {
+                //   highGrade = number;
+                //  }
+                result += number;
+            }
+
+            result = result / grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The higest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }

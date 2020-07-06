d267709e481931f5b337d12a8bb374c4eb167875
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-06 10:33:42 -0500

    adds book.cs

---
+++ gradebook/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace gradebook
+{
+    class Book
+    {
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<double> grades;
+    }
+}

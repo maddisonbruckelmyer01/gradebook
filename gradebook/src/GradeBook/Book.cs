7f0eb6579a10dbb266554395882172a2c7a8b236
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-07 11:43:39 -0500

    cleans up comments and adds properties

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -11,7 +11,28 @@ namespace GradeBook
          {
             grades = new List<double>();
 
-            Name = name; 
+            Name = name;  
+        }
+
+        public void AddGrade(char letter) 
+        {
+           switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                //default:
+                  //  AddGrade(0);
+            }
         }
 
         public void AddGrade(double grade)
@@ -22,7 +43,7 @@ namespace GradeBook
             }
             else 
             {
-                Console.WriteLine("Invalid Value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
@@ -36,58 +57,7 @@ namespace GradeBook
             result.High = double.MinValue;
 
             result.Low = double.MaxValue;
-
-            //foreach loop
-           /* foreach (double grade in grades)
-            {
-                result.Low = Math.Min(grade, result.Low);
-
-                result.High = Math.Max(grade, result.High);
-             
-                result.Average += grade;
-            }
-
-            result.Average /= grades.Count;
-
-            return result; */
-
-
-            //do while loop
-           /* var index = 0;
-             do
-            {
-                result.Low = Math.Min(grades[index], result.Low);
-
-                result.High = Math.Max(grades[index], result.High);
-             
-                result.Average += grades[index];
-
-                index += 1; 
-            } while(index < grades.Count); 
-
-            result.Average /= grades.Count;
-
-            return result; */
-
-
-            //while loop
-             /*  var index = 0;
-             while(index < grades.Count);
-            {
-                result.Low = Math.Min(grades[index], result.Low);
-
-                result.High = Math.Max(grades[index], result.High);
-             
-                result.Average += grades[index];
-
-                index += 1; 
-            } 
-
-            result.Average /= grades.Count;
-
-            return result; */
-
-
+                
             //for loop
              for(var index = 0; index < grades.Count; index +=1)
             {
@@ -101,10 +71,37 @@ namespace GradeBook
 
             result.Average /= grades.Count;
 
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                 case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                 case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                 default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
 
         private List<double> grades;
-        public string Name;
+
+         public string Name 
+         {
+            get;
+            private set;
+          }
+       
+        readonly string category = "Science";
+
     }
 }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,13 +9,46 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Scott's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            var done = false;
+
+            while(!done)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                    {
+                    done = true;
+                    continue;
+                }
+
+                try 
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+                }
+                catch(ArgumentException ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
             var stats = book.GetStatistics();
+            book.Name = "";
+
+            Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter is {stats.Letter}");
         }
     }
 }
